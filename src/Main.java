24ddbb0d521f5e64aeefca25afdcf5867cb9c76a
Edison MM <edisonmonsalve21@gmail.com>
2026-02-15 11:20:34 -0500

    commit operators

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,11 @@
      */
 
 void main() {
-    IO.println("I like mango!");
-    IO.println("It's really good");
-    IO.println("Buy me mango!");
-    Variables.saludar();
-    Input.scanner();
-    MadLibsGame.main();
+//    IO.println("I like mango!");
+//    IO.println("It's really good");
+//    IO.println("Buy me mango!");
+//    Variables.saludar();
+//    Input.scanner();
+//    MadLibsGame.main();
     Operators.main();
 }
--- src/Operators.java
+++ src/Operators.java
@@ -3,16 +3,34 @@ public class Operators {
 
         // Augmented Assignment Operators
 
-        int x = 30;
-        int y = 10;
+//        int x = 30;
+//        int y = 10;
 
+//        x = x + y;
 //        x += y;
+
+//        x = x - y;
 //        x -= y;
+
+//        x = x * y;
 //        x *= y;
-        x = x / y;
+
+//        x = x / y;
 //        x /= y;
 
-        System.out.println(x);
+//        x = x % y;
+//        x %= y;
+
+        // Increment and decrement Operators
+        int x = 1;
+        x++;
+//        x++;
+        x--;
+        IO.println(x);
+
+        // ORDER OF OPERATION P E M D A S () ** * / + -
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        IO.println(result);
 
     }
 
